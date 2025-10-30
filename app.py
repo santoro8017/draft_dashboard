62b7379d563c69ba9cc026bf3125547f897282ec
matt <msantoro17@hotmail.com>
2025-10-29 21:17:34 -0400

    randomized draft order and added a big board

--- app.py
+++ app.py
@@ -4,31 +4,16 @@ from openpyxl import load_workbook
 import os
 from streamlit_autorefresh import st_autorefresh
 
-def reset_players():
-    source_sheet = workbook["original_players"]
-    destination_sheet = workbook["available_players"]
+def reset():
+    new_workbook = load_workbook("draft_data_original.xlsx")    
 
-    for row_index, row in enumerate(source_sheet.iter_rows()):
-        for col_index, cell in enumerate(row):
-            destination_sheet.cell(row=row_index + 1, column=col_index + 1).value = cell.value
-
-
-    workbook.save("draft_data.xlsx")
-
-    return
-
-def reset_teams():
-    for team in get_teams():
-        del workbook[team]
-        workbook.create_sheet(team)
-        
-    workbook.save("draft_data.xlsx")
+    new_workbook.save("draft_data.xlsx")
 
     return
 
 def update_available_players_sheet(df):
 
-    with pd.ExcelWriter("draft_data.xlsx", mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+    with pd.ExcelWriter("draft_data.xlsx", mode='a', engine='openpyxl', header = False, if_sheet_exists='replace') as writer:
         df.to_excel(writer, sheet_name="available_players", index=False)
 
     return
@@ -38,6 +23,31 @@ def get_available_players():
     df = pd.read_excel("draft_data.xlsx", sheet_name="available_players")
 
     return df
+
+def get_draft_order():
+    order = pd.read_excel("draft_data.xlsx", sheet_name="draft_order")
+    
+    if 'Order' in order.columns:
+        return order['Order'].tolist()
+    else:
+        return None
+
+def get_big_board():
+    order = get_draft_order()
+    print(order)
+    if order:
+        teams = order
+    else:
+        teams = get_teams()
+
+    big_board = pd.read_excel("draft_data.xlsx", sheet_name="big_board")
+
+    for team in teams:
+        p = pd.read_excel("draft_data.xlsx", sheet_name=team)
+        p.rename(columns={'Player':team}, inplace=True)
+        big_board = pd.merge(how = 'outer', left=big_board, right = p[['Round',team]], left_on="Round", right_on="Round")
+    
+    return big_board
 
 def get_team_players(team):
     df = pd.read_excel("draft_data.xlsx", sheet_name=team)
@@ -49,63 +59,58 @@ def get_team_players(team):
 
 def get_teams():
     teams = workbook.sheetnames
-    return teams[2:]
+    return teams[3:]
+
+def get_row_num(sheet):
+    for row_num in range(1, sheet.max_row + 2):  # +2 to check beyond max_row
+        cell_value = sheet.cell(row=row_num, column=2).value # column 2 is 'B'
+        if cell_value is None:
+            return row_num
+    
+    return None
+
+import random
+
+def randomize():
+    teams = get_teams()
+    random.shuffle(teams)
+    draft_order = pd.DataFrame(data = {'Order':teams})
+
+    with pd.ExcelWriter("draft_data.xlsx", mode='a', engine='openpyxl', if_sheet_exists='replace') as writer:
+        draft_order.to_excel(writer, sheet_name="draft_order", index=False)
+
+    return 
 
 def update_team(team, Grade, MW, player):
     sheet = workbook[team]
     
-    if not sheet['A1'].value:
-        for col_idx, value in enumerate(["Grade", "MW", "Player"], start=1):
-             sheet.cell(row=1, column=col_idx, value=value)
+    row_num = get_row_num(sheet)
+                
+    if row_num != None:
+        for col_idx, value in enumerate([Grade, MW, player], start=2):
+             sheet.cell(row=row_num, column=col_idx, value=value)
 
-    sheet.append([Grade, MW, player])
-    workbook.save("draft_data.xlsx")
+        workbook.save("draft_data.xlsx")
+    else:
+        st.error("Error with updating team")
 
     return
 
 import time
 
-def get_mod_time(filepath):
-    """Reads file content and its last modification timestamp."""
-    try:
-        last_modified_time = os.path.getmtime(filepath)
-        st.info(f"time: {time.time()}")
-        st.info(last_modified_time)
-        return last_modified_time
-    except FileNotFoundError:
-        st.info("error")
-        return None
-
 ######
-st_autorefresh(interval=3000, key="data_refresh")
+# st_autorefresh(interval=3000, key="data_refresh")
+st.button("Reset", on_click=reset)
+st.button("Randomize Draft Order", on_click=randomize)
 
-# FILE_TO_MONITOR = "draft_data.xlsx"
-# if 'last_mod_time' not in st.session_state:
-#     st.session_state.last_mod_time = os.path.getmtime(FILE_TO_MONITOR) if os.path.exists(FILE_TO_MONITOR) else None
-
-# current_mod_time = get_mod_time(FILE_TO_MONITOR)
-
-# st.info(f"Current_mod_time: {current_mod_time}")
-# st.info(f"last_mod_time: {st.session_state.last_mod_time}")
-# st.info(current_mod_time and st.session_state.last_mod_time)
-# st.info(current_mod_time and st.session_state.last_mod_time and current_mod_time)
-# if current_mod_time and st.session_state.last_mod_time and current_mod_time > st.session_state.last_mod_time:
-#     st.session_state.last_mod_time = current_mod_time
-#     st.info("should be rerun")
-#     st.rerun() # This will rerun the script
-# elif current_mod_time and not st.session_state.last_mod_time: # File created after app started
-#     st.session_state.last_mod_time = current_mod_time
-#     st.info("will be rerun")
-#     st.rerun()
-
-st.button("Reset Available Players", on_click=reset_players)
-st.button("Reset Teams", on_click=reset_teams)
 
 # Create team lists and DataFrames based on the user input
 workbook = load_workbook("draft_data.xlsx")
 
-# Display available players table
-st.dataframe(get_available_players())
+# Display big board
+st.dataframe(get_big_board(), height = 300)
+
+
 
 # Player selection and assignment
 player = st.selectbox('Select player:', get_available_players()["Player"].sort_values(ascending = True).values)
@@ -116,7 +121,7 @@ if st.button('Assign player'):
     Grade = available_players.query("Player == @player")["Grade"].values[0]
     MW = available_players.query("Player == @player")["MW"].values[0]
     player_info = pd.DataFrame(data={"Grade":Grade, "MW": MW, "Player": [player]})
-
+    print(player_info)
     update_team(team, Grade, MW, player)
 
     # Remove assigned player from available players DataFrame
@@ -124,6 +129,9 @@ if st.button('Assign player'):
     update_available_players_sheet(available_players)
 
     st.rerun()
+
+# Display available players table
+st.dataframe(get_available_players())
 
 # Display assigned players tables for each team
 for team in get_teams():
